e3ea3e7eaa300860ee6c6589d70364bf9502832d
Yo Park <yo.park93@yahoo.com>
2020-01-24 04:25:30 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,53 @@
+
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data[:color].each do |key, value|
+    
+    value.each do |name|
+      if new_hash[name.to_s]
+        new_hash[name.to_s][:color] << key.to_s
+      else
+        new_hash[name.to_s] = {
+          :color => [key.to_s]
+        }
+      end
+  end
 end
+  data[:gender].each do |key, value|
+      
+    value.each do |name|
+      
+      if new_hash[name]
+        new_hash[name.to_s][:gender] = []
+        new_hash[name.to_s][:gender] << key.to_s
+      else
+          new_hash[name.to_s] = {
+            :gender => [key.to_s]
+        }
+    end
+end
+end
+data[:lives].each do |key, value|
+  
+  value.each do |name|
+    
+    if new_hash[name]
+      new_hash[name.to_s][:lives] = []
+      new_hash[name.to_s][:lives] << key.to_s
+     
+    else
+      new_hash[name.to_s] = {
+        :lives => [key.to_s]
+      }
+      
+  end
+end
+end
+ return new_hash
+ 
+end
+
+
